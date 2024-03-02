9ee95958f6af63a1d29ae83442483d711d4754c5
adelhpour <ad.heydarabadi@gmail.com>
2024-03-02 12:16:05 -0800

    - extra user info is added to the prompt
    - prompt sections are put in individual sentences
    
    - tests are added for the new changes

--- test_scripts/test_user_info.py
+++ test_scripts/test_user_info.py
@@ -2,14 +2,33 @@ import unittest
 import user_info
 
 class MyTestCase(unittest.TestCase):
-    def test_age(self):
+    def test_age_prompt(self):
         info = user_info.UserInfo(1)
         info.set_user_birthdate("1990-01-01")
-        self.assertEqual(info.populate_user_info(), "We have a 34 years old patient")
+        self.assertEqual(info.populate_age_info(), "They are 34 years old.")
         info = user_info.UserInfo(1)
         info.set_user_birthdate("2023-01-01")
-        self.assertEqual(info.populate_user_info(), "We have a 1 year old patient")
+        self.assertEqual(info.populate_age_info(), "They are 1 year old.")
 
+    def test_height_prompt(self):
+        info = user_info.UserInfo(1)
+        info.set_user_height(180)
+        self.assertEqual(info.populate_height_info(), "They are 180 cm tall.")
+
+    def test_weight_prompt(self):
+        info = user_info.UserInfo(1)
+        info.set_user_weight(80)
+        self.assertEqual(info.populate_weight_info(), "They weigh 80 kg.")
+
+    def test_calorie_intake_prompt(self):
+        info = user_info.UserInfo(1)
+        info.set_user_daily_calorie_intake(2500)
+        self.assertEqual(info.populate_calorie_consumption_info(), "On average, this patient consumes 2500 calories per day.")
+
+    def test_calorie_burn_prompt(self):
+        info = user_info.UserInfo(1)
+        info.set_user_daily_calorie_burn(2000)
+        self.assertEqual(info.populate_calorie_burn_info(), "On average, this patient burns 2000 calories per day.")
 
 if __name__ == '__main__':
     unittest.main()
